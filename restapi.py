737b3f168443b1a6e0dba83f2b61eadaafe39efe
daan Lockhorst <daanlockhorst@hotmail.com>
2018-03-23 17:23:28 +0100

    Add files via upload

--- restapi.py
+++ restapi.py
@@ -2,38 +2,33 @@ import flask
 import json
 from werkzeug.utils import secure_filename
 import jsonify
-
 from flask import Flask, jsonify
-
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 data = json.load(open('data.json'))
 customers = [data]
 
-@app.route('/json/<int:cust_id>', methods=['GET'])
-def get_cust(cust_id):
-    cust = [cust for cust in customers if cust['id'] == cust_id]
-    if len(cust) == 0:
-        abort(404)
-    return jsonify ({'cust': cust[0]})
+@app.route('/get_json', methods=['GET'])
+def get_data():
+    return jsonify (customers)
 
+@app.route('/get_json_file/<path:path>')
+def send_js(path):
+    return send_from_directory('/', path)
 
-@app.route('/put_json/', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'movements': request.json['movements'],
-        'searches': request.json['searches'],
-        'input devices': request.json['input devices'],
-        'app usage': request.json['app usage']
-    }
-    tasks.append(task)
-    database.insert_into()
-    return jsonify({'task': task}), 201
+@app.route('/post_json_file', methods=['POST'])
+def post_json(path):
+    print ('posted')
 
+if __name__ == "__main__":
+    app.run()
+def get(path):
+    return send_from_directory('js', path)
 
+if __name__ == "__main__":
+    app.run()
+
+    
 @app.errorhandler(404)
 def not_found(error):
     return flask.make_response(jsonify({'error': 'Not found'}), 404)
