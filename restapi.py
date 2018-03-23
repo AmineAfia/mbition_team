c545e02a9384a65c1bf7bce29000715fce523b58
daan <daanlockhorst@hotmail.com>
2018-03-23 18:30:26 +0100

    serving on localhost 5000 /index.html

--- restapi.py
+++ restapi.py
@@ -1,37 +1,33 @@
-import flask
-import json
-from werkzeug.utils import secure_filename
+from flask import Flask, request, Response, send_from_directory
 import jsonify
-from flask import Flask, jsonify
-app = Flask(__name__, static_url_path='')
+import os.path
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__)
 
-data = json.load(open('data.json'))
-customers = [data]
 
-@app.route('/get_json', methods=['GET'])
-def get_data():
-    return jsonify (customers)
 
-@app.route('/get_json_file/<path:path>')
-def send_js(path):
-    return send_from_directory('/', path)
+@app.route('/a', methods=['GET'])   
+def send_html():
+    content = open('static-frontend/index.html')
+    return Response(content, mimetype="text/html")
 
-@app.route('/post_json_file', methods=['POST'])
-def post_json(path):
-    print ('posted')
+@app.route('/b/<path:path>')
+def static_proxy(path):
+  return app.send_static_file(path)
 
-if __name__ == "__main__":
-    app.run()
-def get(path):
-    return send_from_directory('js', path)
 
-if __name__ == "__main__":
-    app.run()
+
+
+@app.route('/<path:filename>', methods=['GET', 'POST'])
+def index(filename):
+    filename = filename or 'index.html'
+    if request.method == 'GET':
+        return send_from_directory('/Users/daanlock/Desktop/mbiti/mbition_team/static-frontend/', filename)
+
+    return jsonify(request.data)
+   
 
     
-@app.errorhandler(404)
-def not_found(error):
-    return flask.make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)(debug = True)
